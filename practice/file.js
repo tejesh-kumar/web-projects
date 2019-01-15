b47fb8379ffc4014193f68fe810abd5dca7720bc
Tejesh Kumar <tejeshmuralikrishna@gmail.com>
2019-01-15 13:54:06 +0530

    unwanted files

--- practice/file.js
+++
@@ -1,8 +0,0 @@
-function fun(a)
-{var b=0;
- document.form1.text1.value=a;
- b=a; }
-function fun1()
-{var a =document.form1.text1.value=5; }
-function fun2()
-{var a =document.form1.text1.value=5; }
